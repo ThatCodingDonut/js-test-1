fc8e2f3c7ff66bcb8958dba11015841ce6aaf28c
ThatCodingDonut <corbinchandler777@icloud.com>
2025-04-10 11:10:30 -0400

    main

--- index.js
+++ index.js
@@ -2,19 +2,55 @@ import express from 'express';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
+import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 80;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname)));
 
 // Route to serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+  res.render(path.join(__dirname, 'pages', 'index.ejs'));
 });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'register', 'register.html'))
+app.all('/register', (req, res) => {
+    if (req.method == 'GET') {
+        res.render(path.join(__dirname, 'pages', 'register', 'register.ejs'))
+    } else {
+        res.send(`
+        <h1 style="text-align: center;">Thank you for applying!</h1>
+        `)
+        var MailClient = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "corbinchandler727@gmail.com",
+                pass: process.env.APP_PASS
+            },
+            secure: true,
+            port: 465,
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+
+        MailClient.sendMail({
+            subject: "Hi!",
+            from: "Corbin C <corbinchandler727@gmail.com>",
+            to: "corbinchandler777@gmail.com",
+            text: "Hello!"
+        })
+
+        console.log("USER REGISTERED")
+    }
 })
 
 // Start the server
